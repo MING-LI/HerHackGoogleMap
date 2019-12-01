f7a7ad5c11db0e4bd64aaba2354241163aa94987
li ken <cielcava@gmail.com>
2019-12-01 19:35:01 +0800

    Initial Commit

---
+++ HerHeckGoogleMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HerHeckGoogleMap
+//
+//  Created by Ken Li on 1/12/2019.
+//  Copyright © 2019 pdni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
